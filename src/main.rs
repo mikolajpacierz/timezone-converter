ae81a020f881c4b760feef901d5485597e3e045d
Mikołaj Pacierz <mikolajpacierz18@gmail.com>
2026-04-23 15:39:03 +0200

    feat: added argument for target timezone

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,19 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short, long, required = true)]
     date: String,
-    #[arg(short, long)]
+    #[arg(short, long, required = true)]
     time: String,
-    #[arg(short, long)]
+    #[arg(short, long, required = false)]
     abbr: String,
+    #[arg(short, long, default_value = "Europe/Warsaw")]
+    local_timezone: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let date = args.date;
-    let time = args.time;
-    let abbr = &args.abbr;
-    let datetime = format!("{date} {time}");
-    println!("{}", convert_time(&datetime, abbr, "Europe/Warsaw"));
+    let datetime = format!("{} {}", args.date, args.time);
+    println!("{}", convert_time(&datetime, &args.abbr, &args.local_timezone));
 }
